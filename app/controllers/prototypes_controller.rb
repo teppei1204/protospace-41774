6baa5502b778c70d39e16b7480a37a7e97c884b4
teppei1204 <m-n-m09@docomo.ne.jp>
2024-12-09 14:22:04 +0900

    修正９

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,8 +1,7 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   before_action :set_prototype, only: [:edit, :update, :destroy, :show]
-  before_action :move_to_index, except: [:index, :show,:edit, :update, :destroy]
-
+  before_action :authorize_user, only: [:edit, :update, :destroy, :show]
 
   def index
     @prototypes = Prototype.includes(:user)
@@ -11,51 +10,43 @@ class PrototypesController < ApplicationController
   def new
     @prototype = Prototype.new
   end
-  
+
   def create
     @prototype = current_user.prototypes.new(prototype_params)
     if @prototype.save
       redirect_to root_path
     else
-      render :new
+      render :new, status: :unprocessable_entity
     end
   end
 
-
-
-
   def show
-    @prototype = Prototype.find(params[:id])
     @comment = Comment.new
     @comments = @prototype.comments.includes(:user)
   end
 
   def edit
-    @prototype = Prototype.find(params[:id])
+    
   end
 
   def update
-      @prototype = Prototype.find(params[:id])
     if @prototype.update(prototype_params)
       redirect_to @prototype
     else
-      render :edit
+      render :edit, status: :unprocessable_entity
     end
   end
 
   def destroy
-    @prototype = Prototype.find(params[:id])
     @prototype.destroy
     redirect_to root_path
   end
 
-
   private
-  
+
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
-end
 
   def set_prototype
     @prototype = Prototype.find(params[:id])
@@ -72,3 +63,4 @@ end
       redirect_to root_path
     end
   end
+end
